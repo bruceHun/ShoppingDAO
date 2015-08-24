813ae10f1fb58b557a43d79f3ae7ee43790eeb6e
bruceHun <bhung2011@gmail.com>
2015-08-24 14:16:45 +0800

    Mysqlconn use db.properties

--- src/shopping/Business/MySQLconn.java
+++ src/shopping/Business/MySQLconn.java
@@ -1,25 +1,40 @@
 package shopping.Business;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class MySQLconn {
 	public static Connection getConnection() throws SQLException {
         Connection conn = null;
  
         String driverName = "com.mysql.jdbc.Driver";
-        String connString = "jdbc:mysql://localhost:3306/Shopping?"
-                + "user=root&password=";
-        try {
+        
+        /*String connString = "jdbc:mysql://localhost:3306/Shopping?"
+                + "user=root&password=";*/
+        try (FileInputStream f = new FileInputStream("db.properties")){
             Class.forName(driverName);
-            conn = DriverManager.getConnection(connString);
+            Properties pros = new Properties();
+            pros.load(f);
+            
+            String url = pros.getProperty("url");
+            String user = pros.getProperty("user");
+            String password = pros.getProperty("password");
+            
+            conn = DriverManager.getConnection(url,user,password);
 
+            
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         } catch (SQLException e) {
             System.out.println(e);
-        }
+        } catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         return conn;
     }
 }
